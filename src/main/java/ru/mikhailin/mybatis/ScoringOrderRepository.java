11091b3e53edf5a02d17ffe29e828a9c53b12401
denismixy <psp3000d@mail.ru>
2024-05-29 13:38:04 +0500

    Update

--- src/main/java/ru/mikhailin/mybatis/ScoringOrderRepository.java
+++ src/main/java/ru/mikhailin/mybatis/ScoringOrderRepository.java
@@ -2,7 +2,6 @@ package ru.mikhailin.mybatis;
 
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
-import org.apache.ibatis.annotations.Options;
 import org.apache.ibatis.annotations.Select;
 import ru.mikhailin.entity.ScoringOrder;
 
