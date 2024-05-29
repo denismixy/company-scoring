8ced366b6a0a7d0b18bfa70330b2065abd5a1059
denismixy <psp3000d@mail.ru>
2024-05-29 13:53:56 +0500

    Update

--- src/main/java/ru/mikhailin/mybatis/ScoringOrderRepository.java
+++ src/main/java/ru/mikhailin/mybatis/ScoringOrderRepository.java
@@ -3,17 +3,11 @@ package ru.mikhailin.mybatis;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Options;
-import org.apache.ibatis.annotations.Select;
 import ru.mikhailin.entity.ScoringOrder;
-
-import java.util.List;
 
 @Mapper
 public interface ScoringOrderRepository {
     @Insert("INSERT INTO SCORING_ORDERS(INN, REGION, CAPITAL) VALUES (#{inn}, #{region}, #{capital})")
     @Options(useGeneratedKeys = true, keyProperty = "id", keyColumn = "ID")
-    Integer insertScoringOrder(ScoringOrder scoringOrder);
-
-    @Select("SELECT * FROM SCORING_ORDERS WHERE ID = ")
-    List<ScoringOrder> getOrders();
+    void insertScoringOrder(ScoringOrder scoringOrder);
 }
